97c5922e3c58268847005d17800bde04c8042a7d
emreavcia <avciemrea@gmail.com>
2022-01-12 19:34:38 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,138 @@
+const mainData = [
+    {
+        marco: {
+            matematik: 80,
+            fizik: 90,
+            kimya: 80,
+            geometri: 80
+        },
+        alessandra: {
+            matematik: 80,
+            fizik: 80,
+        },
+        Isabella: {
+            matematik: 80,
+            fizik: 80,
+            kimya: 80,
+            geometri: 80
+        },
+        elisa: {
+            matematik: 80,
+            fizik: 80,
+            kimya: 80,
+            geometri: 80
+        },
+        rafale: {
+            matematik: 80,
+            fizik: 70,
+            kimya: 80,
+        },
+        hans: {
+            matematik: 80,
+            fizik: 80,
+            kimya: 80,
+            geometri: 80
+        },
+        jack: {
+            matematik: 80,
+            fizik: 80,
+            kimya: 80,
+            geometri: 80
+        },
+        vincent: {
+            matematik: 80,
+            fizik: 80,
+            kimya: 80,
+            geometri: 80
+        }
+    },
+    {
+        marco: {
+            matematik: 80,
+            fizik: 90,
+            kimya: 80,
+            geometri: 80
+        },
+        alessandra: {
+            matematik: 80,
+            fizik: 80,
+        },
+        Isabella: {
+            matematik: 80,
+            fizik: 80,
+            kimya: 80,
+            geometri: 80
+        },
+    },
+    {
+        marco: {
+            matematik: 80,
+            fizik: 80,
+            kimya: 80,
+            geometri: 80
+        },
+        alessandra: {
+            matematik: 80,
+            fizik: 80,
+        },
+        Isabella: {
+            matematik: 80,
+            fizik: 80,
+            kimya: 80,
+            geometri: 80
+        },
+        elisa: {
+            matematik: 80,
+            fizik: 80,
+            kimya: 80,
+            geometri: 80
+        },
+        rafale: {
+            matematik: 80,
+            fizik: 70,
+            kimya: 80,
+        },
+        hans: {
+            matematik: 80,
+            fizik: 80,
+            kimya: 80,
+            geometri: 80
+        },
+        jack: {
+            matematik: 80,
+            fizik: 80,
+            kimya: 80,
+            geometri: 80
+        },
+        vincent: {
+            matematik: 80,
+            fizik: 80,
+            kimya: 80,
+            geometri: 80
+        },
+        rafale: {
+            matematik: 80,
+            fizik: 70,
+            kimya: 80,
+        },
+        hans: {
+            matematik: 80,
+            fizik: 80,
+            kimya: 80,
+            geometri: 80
+        },
+        jack: {
+            matematik: 80,
+            fizik: 80,
+            kimya: 80,
+            geometri: 80
+        },
+        vincent: {
+            matematik: 80,
+            fizik: 80,
+            kimya: 80,
+            geometri: 80
+        }
+    }
+]
+

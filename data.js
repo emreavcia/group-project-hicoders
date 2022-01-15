12425e1c226ebd17b95e07d1de1f75e27bf954b1
emreavcia <avciemrea@gmail.com>
2022-01-16 00:29:15 +0100

    one step further for a single page application

--- data.js
+++ data.js
@@ -1,141 +1,357 @@
 const mainData = [
-    classA = {
-        marco: {
-            matematik: 80,
-            fizik: 90,
-            kimya: 80,
-            geometri: 80
-        },
-        alessandra: {
-            matematik: 80,
-            fizik: 80,
-        },
-        Isabella: {
-            matematik: 80,
-            fizik: 80,
-            kimya: 80,
-            geometri: 80
-        },
-        elisa: {
-            matematik: 80,
-            fizik: 80,
-            kimya: 80,
-            geometri: 80
-        },
-        rafale: {
-            matematik: 80,
-            fizik: 70,
-            kimya: 80,
-        },
-        hans: {
-            matematik: 80,
-            fizik: 80,
-            kimya: 80,
-            geometri: 80
-        },
-        jack: {
-            matematik: 80,
-            fizik: 80,
-            kimya: 80,
-            geometri: 80
-        },
-        vincent: {
-            matematik: 80,
-            fizik: 80,
-            kimya: 80,
-            geometri: 80
-        }
-    },
+    {
+        className: "Class A",
+        students: [
+            {
+                studentName: "AMarko",
+                subject: [
+                    {
+                        subjectName: "Matematik",
+                        grade: 80
+                    },
+                    {
+                        subjectName: "Fizik",
+                        grade: 70
+                    },
+                    {
+                        subjectName: "Kimya",
+                        grade: 80
+                    },
 
-    classB = {
-        marco: {
-            matematik: 80,
-            fizik: 90,
-            kimya: 80,
-            geometri: 80
-        },
-        alessandra: {
-            matematik: 80,
-            fizik: 80,
-        },
-        Isabella: {
-            matematik: 80,
-            fizik: 80,
-            kimya: 80,
-            geometri: 80
-        },
+                ]
+            },
+            {
+                studentName: "AEmre",
+                subject: [
+                    {
+                        subjectName: "Matematik",
+                        grade: 80
+                    },
+                    {
+                        subjectName: "Fizik",
+                        grade: 70
+                    },
+                    {
+                        subjectName: "Geometri",
+                        grade: 80
+                    }
+                ]
+            },
+            {
+                studentName: "AMemre",
+                subject: [
+                    {
+                        subjectName: "Matematik",
+                        grade: 80
+                    },
+                    {
+                        subjectName: "Fizik",
+                        grade: 70
+                    },
+                    {
+                        subjectName: "Kimya",
+                        grade: 80
+                    },
+                    {
+                        subjectName: "Geometri",
+                        grade: 80
+                    }
+                ]
+            },
+        ],
     },
-    classC = {
-        marco: {
-            matematik: 80,
-            fizik: 80,
-            kimya: 80,
-            geometri: 80
-        },
-        alessandra: {
-            matematik: 80,
-            fizik: 80,
-        },
-        Isabella: {
-            matematik: 80,
-            fizik: 80,
-            kimya: 80,
-            geometri: 80
-        },
-        elisa: {
-            matematik: 80,
-            fizik: 80,
-            kimya: 80,
-            geometri: 80
-        },
-        rafale: {
-            matematik: 80,
-            fizik: 70,
-            kimya: 80,
-        },
-        hans: {
-            matematik: 80,
-            fizik: 80,
-            kimya: 80,
-            geometri: 80
-        },
-        jack: {
-            matematik: 80,
-            fizik: 80,
-            kimya: 80,
-            geometri: 80
-        },
-        vincent: {
-            matematik: 80,
-            fizik: 80,
-            kimya: 80,
-            geometri: 80
-        },
-        rafale: {
-            matematik: 80,
-            fizik: 70,
-            kimya: 80,
-        },
-        hans: {
-            matematik: 80,
-            fizik: 80,
-            kimya: 80,
-            geometri: 80
-        },
-        jack: {
-            matematik: 80,
-            fizik: 80,
-            kimya: 80,
-            geometri: 80
-        },
-        vincent: {
-            matematik: 80,
-            fizik: 80,
-            kimya: 80,
-            geometri: 80
-        }
+    {
+        className: "Class B",
+        students: [
+            {
+                studentName: "BMarko",
+                subject: [
+                    {
+                        subjectName: "Matematik",
+                        grade: 20
+                    },
+                    {
+                        subjectName: "Fizik",
+                        grade: 40
+                    },
+                    {
+                        subjectName: "Geometri",
+                        grade: 10
+                    }
+                ]
+            },
+            {
+                studentName: "BTarko",
+                subject: [
+                    {
+                        subjectName: "Matematik",
+                        grade: 90
+                    },
+                    {
+                        subjectName: "Fizik",
+                        grade: 60
+                    },
+                    {
+                        subjectName: "Kimya",
+                        grade: 100
+                    },
+                    {
+                        subjectName: "Geometri",
+                        grade: 10
+                    }
+                ]
+            },
+        ],
+    },
+    {
+        className: "Class C",
+        students: [
+            {
+                studentName: "CTarko",
+                subject: [
+                    {
+                        subjectName: "Fizik",
+                        grade: 70
+                    },
+                    {
+                        subjectName: "Kimya",
+                        grade: 25
+                    },
+                    {
+                        subjectName: "Geometri",
+                        grade: 43
+                    }
+                ]
+            },
+            {
+                studentName: "CSlaas ",
+                subject: [
+                    {
+                        subjectName: "Matematik",
+                        grade: 15
+                    },
+                    {
+                        subjectName: "Fizik",
+                        grade: 75
+                    },
+                    {
+                        subjectName: "Geometri",
+                        grade: 80
+                    }
+                ]
+            },
+            {
+                studentName: "CTarko",
+                subject: [
+                    {
+                        subjectName: "Matematik",
+                        grade: 90
+                    },
+                    {
+                        subjectName: "Fizik",
+                        grade: 70
+                    },
+                    {
+                        subjectName: "Kimya",
+                        grade: 80
+                    },
+                    {
+                        subjectName: "Geometri",
+                        grade: 80
+                    }
+                ]
+            },
+            {
+                studentName: "CTarko",
+                subject: [
+                    {
+                        subjectName: "Matematik",
+                        grade: 90
+                    },
+                    {
+                        subjectName: "Fizik",
+                        grade: 70
+                    },
+                    {
+                        subjectName: "Kimya",
+                        grade: 80
+                    },
+                    {
+                        subjectName: "Geometri",
+                        grade: 80
+                    }
+                ]
+            },
+            {
+                studentName: "CTarko",
+                subject: [
+                    {
+                        subjectName: "Matematik",
+                        grade: 90
+                    },
+                    {
+                        subjectName: "Fizik",
+                        grade: 70
+                    },
+                    {
+                        subjectName: "Kimya",
+                        grade: 80
+                    },
+                    {
+                        subjectName: "Geometri",
+                        grade: 80
+                    }
+                ]
+            },
+        ]
     }
 ]
 
 
 
+
+
+
+
+
+/*  marco: {
+     matematik: 80,
+     fizik: 90,
+     kimya: 80,
+     geometri: 80
+ },
+ alessandra: {
+     matematik: 80,
+     fizik: 80,
+ },
+ Isabella: {
+     matematik: 80,
+     fizik: 80,
+     kimya: 80,
+     geometri: 80
+ },
+ elisa: {
+     matematik: 80,
+     fizik: 80,
+     kimya: 80,
+     geometri: 80
+ },
+ rafale: {
+     matematik: 80,
+     fizik: 70,
+     kimya: 80,
+ },
+ hans: {
+     matematik: 80,
+     fizik: 80,
+     kimya: 80,
+     geometri: 80
+ },
+ jack: {
+     matematik: 80,
+     fizik: 80,
+     kimya: 80,
+     geometri: 80
+ },
+ vincent: {
+     matematik: 80,
+     fizik: 80,
+     kimya: 80,
+     geometri: 80
+ }
+},
+
+
+{
+ marco: {
+     matematik: 80,
+     fizik: 90,
+     kimya: 80,
+     geometri: 80
+ },
+ alessandra: {
+     matematik: 80,
+     fizik: 80,
+ },
+ Isabella: {
+     matematik: 80,
+     fizik: 80,
+     kimya: 80,
+     geometri: 80
+ },
+},
+{
+ marco: {
+     matematik: 80,
+     fizik: 80,
+     kimya: 80,
+     geometri: 80
+ },
+ alessandra: {
+     matematik: 80,
+     fizik: 80,
+ },
+ Isabella: {
+     matematik: 80,
+     fizik: 80,
+     kimya: 80,
+     geometri: 80
+ },
+ elisa: {
+     matematik: 80,
+     fizik: 80,
+     kimya: 80,
+     geometri: 80
+ },
+ rafale: {
+     matematik: 80,
+     fizik: 70,
+     kimya: 80,
+ },
+ hans: {
+     matematik: 80,
+     fizik: 80,
+     kimya: 80,
+     geometri: 80
+ },
+ jack: {
+     matematik: 80,
+     fizik: 80,
+     kimya: 80,
+     geometri: 80
+ },
+ vincent: {
+     matematik: 80,
+     fizik: 80,
+     kimya: 80,
+     geometri: 80
+ },
+ rafale: {
+     matematik: 80,
+     fizik: 70,
+     kimya: 80,
+ },
+ hans: {
+     matematik: 80,
+     fizik: 80,
+     kimya: 80,
+     geometri: 80
+ },
+ jack: {
+     matematik: 80,
+     fizik: 80,
+     kimya: 80,
+     geometri: 80
+ },
+ vincent: {
+     matematik: 80,
+     fizik: 80,
+     kimya: 80,
+     geometri: 80
+ }
+}
+]
+
+
+
+*/
